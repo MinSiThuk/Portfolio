9c474c3c6dea7b93a26cb8cffeb7ef2fb57c9f87
MinSiThuK <minsithu21187@gmail.com>
2024-02-02 22:38:00 +0630

    Repair comit

--- forms/contact.php
+++ forms/contact.php
@@ -18,15 +18,14 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'minsithu21187@gmail.com',
+    'username' => 'Min Si Thu Min',
+    'password' => 'sxoa vsxq bwqt agsc',
+    'port' => '465'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
